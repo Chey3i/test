53f0f83bb28001de764270907fd565084cd70aee
Artem Kum <kum1998@mail.ru>
2023-02-02 15:46:22 +0700

    Тестовое задание

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>       
+    <div :class="[theme ? 'blackTheme' : 'whiteTheme', 'block']" >
+        <div :class="[theme ? 'blackTheme' : 'whiteTheme', 'block1']">
+            <div class="dataTimeExit">
+                <div class="dataTime">
+                    <div>{{dataUser}}</div>
+                    <div>{{timeUser}}</div> 
+                </div>
+                <div class="exit">
+                    <div>Выход</div>
+                    <img :class="{imgExitWhite:theme, imgExitBlack:!theme}" src=".\exitIcon.png" alt="exit">
+                </div>
+            </div>
+            <div class="toggleComponent">
+                <input @click="myFilter" type="checkbox" id="toggle_checkbox">
+                <label for="toggle_checkbox">
+                    <div id="star">
+                        <div class="star" id="star-1">★</div>
+                        <div class="star" id="star-2">★</div>
+                    </div>
+                    <div id="moon"></div>
+                </label>
+            </div>
+            <div class="card">
+
+                <div class="fio">{{fio}}</div>
+                <div class="dateofBirth">{{dateofBirth}}</div>
+                <div class="tabN">Таб №: {{tabN}}</div>
+                <div class="position">{{position}}</div>
+            
+            </div>
+            <div class="information-for-users">
+                <div class="info-box" v-for="userINFO in UserInformations" v-bind:key="userINFO">
+                    <div>{{userINFO.title}}</div>
+                    <div><img class="img1" :src="userINFO.img" alt="1"></div>
+                </div>
+            </div>  
+        </div>
+        <div class="block2">
+            <div class="block3">
+                <div class="move-box" v-for="moveINFO in moveInformations" v-bind:key="moveINFO">
+                    <div>{{moveINFO.title}}</div>
+                    <div><img class="img2" :src="moveINFO.img" alt="1"></div>   
+                </div> 
+            </div>
+            <div class="logo">
+                <img class="logo-img" src="./logo.png" alt="logo">
+            </div>
+        </div>
+    </div>       
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            dataUser : 'XX.XX.XXXX',
+            timeUser : 'XX.XX',
+            theme: true,
+            fio: 'Фамилия Имя Отчество',
+            dateofBirth: 'XX.XX.XXXX',
+            tabN: 'ГОКИ 0000',
+            position: 'Проходчик IV разряда',
+            UserInformations:[
+                {id:1, title:'Инструктаж', img: ''},
+                {id:2, title:'Предсменный экзаменатор', img: ''},
+                {id:3, title:'Тестов выполнено', img: ''},
+                {id:4, title:'Аттестация через', img: ''},
+            ],
+            moveInformations:[
+                {id:1, title:'Начать работу', img: ''},
+                {id:2, title:'Уведомления', img: ''},
+                {id:3, title:'Обучение', img: ''},
+                {id:4, title:'Достижения', img: ''},
+            ],
+           
+        }
+    },
+    methods: {
+        myFilter: function() {this.theme = !this.theme;},}
+}
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App'
+import './styles.css'
+import './toggle.css'
+
+
+
+createApp(App).mount('#app')
